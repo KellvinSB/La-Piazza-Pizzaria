9c2e3c39a937ebfbacd5c03a15bbe8da64ff5387
Kellvin <Kellvin_sbarros@hotmail.com>
2024-08-01 00:36:47 -0300

    feat: add close menu on <li> click and fix CSS bug on resize to large screens

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,9 @@
-let show = true;
 const header = document.querySelector('.header');
 const menu = header.querySelector('.header__hamburger-menu');
+const navItems = header.querySelectorAll('.header__nav-list li a');
 
 menu.addEventListener('click', () => {
-	header.classList.toggle('on', show);
-	show = !show;
+	header.classList.toggle('on');
 });
+
+navItems.forEach(item => item.addEventListener('click', () => header.classList.remove('on')));
